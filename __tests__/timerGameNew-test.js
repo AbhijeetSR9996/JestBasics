87cdd4dfcc237f1a0115cb484913e27df1c3e0cc
AbhijeetSR9996 <sabhijeet191@yahoo.com>
2023-01-28 20:47:35 +0530

    Add all my files

---
+++ __tests__/timerGameNew-test.js
@@ -0,0 +1,17 @@
+jest.useFakeTimers();
+it('calls the callback after 1 second via advanceTimersByTime', () => {
+  const timerGame = require('../src/timerGameNew');
+  const callback = jest.fn();
+
+  timerGame(callback);
+
+  // At this point in time, the callback should not have been called yet
+  expect(callback).not.toBeCalled();
+
+  // Fast-forward until all timers have been executed
+  jest.advanceTimersByTime(1000);
+
+  // Now our callback should have been called!
+  expect(callback).toBeCalled();
+  expect(callback).toHaveBeenCalledTimes(1);
+});
